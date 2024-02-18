697a216f71d5532d1723535c58b7b4118975f688
muhib551 <muhib551@gmail.com>
2024-02-18 18:28:59 +0500

    replaced about us description

--- src/components/AboutUs/Sub/AboutUsCard.tsx
+++ src/components/AboutUs/Sub/AboutUsCard.tsx
@@ -12,38 +12,38 @@ const AboutUsCard = () => {
             <Image src={storyIcon} alt="" />
             <p className="text-[30px]">Established in 1998</p>
           </div>
-          <p className="py-5">
-            Our skilled team members predate the term “Cybersecurity” with
-            decades of experience in “Information Security”!
-            <br />
-            <br />
-            We are a Cyber Security and Risk Management Company that helps
-            organizations become Cyberresllient through Continuous Threat
-            Exposure Management (CTEM), Risk Management, and real-time 24x7
-            Threat Monitoring and Incident Response.
-            <br />
-            <br />
-            Based in Florida and serving clients in North America and the
-            Caribbean.
+          <br />
+          <p className="">
+            We are a Cyber Security and Risk Management company that provides
+            assurance and managed security solutions to organizations
+            nationwide. By developing a continuous threat exposure management
+            strategy, we help organizations reduce their attack surface and
+            protect digital assets from theft and unauthorized access.
           </p>
-          {/* <p className="py-5">
-            InfoSight has digitally transformed clients by achieving unheralded
-            business outcomes through our{" "}
-            <span className="text-gray-600 font-bold">
-              Managed Security & Advisory Services.
-            </span>{" "}
-            From the <span className="text-gray-600 font-bold">Datacenter</span>{" "}
-            to the <span className="text-gray-600 font-bold">Cloud</span>, We{" "}
-            <span className="text-gray-600 font-bold">
-              architect, secure and manage both IT and OT/ICS critical network
-              data 24x7x365.
-            </span>{" "}
-            Our team delivers goal-oriented security assessments to help
-            minimize cyber risk and meet compliance requirements by creating an{" "}
-            <span className="text-gray-600 font-bold">
-              enterprise cyber risk program.
+          <br />
+          <p>
+            Our skilled team members predate the term “Cybersecurity” with
+            decades of experience in “Information Security”! Our experience
+            within Regulated Industry spans over two and half decades brining
+            the experience-based knowledge that helps organizations become Cyber
+            Resilient.
+          </p>
+          <br />
+          <p>
+            With our innovative approach to cyber risk management, organizations
+            rely on us to design, build, and manage their cybersecurity,
+            allowing them to operate securely and achieve an optimized state of
+            cyber defense. Clients depend on us 24x7x365 to provide the highest
+            level of advice, operational security, and incident response.
+          </p>
+          <br />
+          <p>
+            Based in Florida and serving clients in the Americas.{" "}
+            <span className="text-red-600">
+              Cyber Risk Management, 24x7 Threat Monitoring and Incident
+              Response.
             </span>
-          </p> */}
+          </p>
         </div>
       </div>
     </ContentContainer>
